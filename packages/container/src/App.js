876c3c768c94ba407618eb4ef2837c6e77519d1e
Prashanth r <pramakrishna@tekion.com>
2024-11-12 23:26:25 +0530

    add buildflow for marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import MarketingApp from './components/MarketingApp';
+import MarketingApp from './components/MarketingApp';s
 
 export default () => {
     return (
         <div>
             <div>Hello</div>
-            {/* <MarketingApp /> */}
+            <MarketingApp />
         </div>
   )
 }
